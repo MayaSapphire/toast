e4166186854cb30fce4d75f7fe108ebab0c18aaa
MayaSapphire <71132181+MayaSapphire@users.noreply.github.com>
2026-06-24 13:01:58 -0500

    add app logo and modify settings screen

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, View, Alert } from 'react-native';
 
 export default function SettingsScreen() {
+
+  function exportTasks() {
+    Alert.alert('Function not implemented', 'Export tasks functionality is not yet implemented. Please check back later.');
+  }
+
+
+  function importTasks() {
+    Alert.alert('Function not implemented', 'Import tasks functionality is not yet implemented. Please check back later.');
+  }
+
   return (
-    <View style={{
+    <ScrollView style={{
+      marginTop: 10,
       marginLeft: 10,
       marginRight: 10,
       marginBottom:130}}
     >
-      <Text style={styles.titleText}>This screen is a placeholder...for now</Text>
-    </View>
+      <View style={{flexDirection: 'row', justifyContent: 'space-around', paddingHorizontal: 10}}>
+        <Button color="purple" onPress={importTasks} title="Import tasks" />
+        <Button color="purple" onPress={exportTasks} title="Export tasks" />
+      </View>
+    </ScrollView>
   );
 }
 
